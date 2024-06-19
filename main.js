9629446112c8541e4c82147c1f7d8da757e97ba9
munaciella <franes88@gmail.com>
2024-06-19 21:10:34 +0100

    test regex patterns to detect md syntax

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-const fs = require('fs')
-const marked = require('marked')
+const fs = require('fs');
+const marked = require('marked');
 
 const readMarkDownFile = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (error, result) => {
-            if (error) {
-                return reject(error)
-            }
-            resolve(result)
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(result);
+    });
+  });
+};
 
 const main = async () => {
-    try {
-        const filePath = 'node.md'
-        const result =  await readMarkDownFile(filePath)
-        console.log('markdown contents: \n', result);
-        
-    } catch (error) {
-        console.error('Error reading file', error)
-    }
-}
+  let topicOrder = 0;
+  const topicRegex = /#\s\w+(\s\w+)*\s/
+  const tagsRegex = /#{2}\s\w+(\s\w+)*\s/
+  try {
+    let tags = []
+    let topic = '';
+    let red = '';
+    let note = {};
+    const filePath = 'node.md';
+    const result = await readMarkDownFile(filePath);
+    Array.from(result).forEach((character) => {
+      red += character;
+      if (tagsRegex.test(red)) {
+        tags.push(red)
+        red = '';
+      }
+    });
+    //console.log('markdown contents: \n', result);
+    console.log('tags are ', tags);
+  } catch (error) {
+    console.error('Error reading file', error);
+  }
+};
 
-main()
+main();
 
-// TODO Be able to accept json files 
-// TODO Read json files 
 // TODO Create js objects based on json file
+// TODO /^(#{1,6})\s+(.*)$/gm
 // TODO Write new files
+// TODO regex ^(?!#{1,6}\s).+$  - For content
+/// TODO /^(#{1,6})\s+(.*)$/gm - For Headings
